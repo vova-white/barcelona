055bb757066750a6a9b93fd8deafb013a458dd40
vova_white <belyj.vladimir@gmail.com>
2019-08-16 12:51:25 +0200

    taps toggle

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,6 +7,22 @@
       $('.navigation').toggleClass('navigation--is_open');
       $('body').toggleClass('is--scroll_off');
     });
+
+    // Toogle Taps
+    $('.tap__button--js').click(function(e) {
+      e.preventDefault();
+
+      const tapId = $(this).attr('href').replace('#', '');
+      const $tapContent = $(`[data-tap='${tapId}']`);
+
+      $('.tap-content').hide();
+      $tapContent.show();
+    });
+    $('.tap-close').click(function(e) {
+      e.preventDefault();
+
+      $('.tap-content').hide();
+    });
   });
 })(jQuery);
 
