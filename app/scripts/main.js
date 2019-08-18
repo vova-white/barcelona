0cc73275dca6f8f9d901cebb44f3ba305f614a7a
vova_white <belyj.vladimir@gmail.com>
2019-08-18 17:54:35 +0200

    add animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,38 @@
+// ScrollReveal
+const fromBottom = {
+  distance: '50%',
+  origin: 'bottom',
+  duration: 800
+};
+const fromBottomLong = {
+  ...fromBottom, distance: '100%'
+}
+const fromLeft = {
+  distance: '50%',
+  origin: 'left',
+  duration: 800
+};
+const fromRight = {
+  distance: '50%',
+  origin: 'right',
+  duration: 800
+};
+const fromTop = {
+  distance: '50%',
+  origin: 'top',
+  duration: 800
+}
+ScrollReveal().reveal('.from-left', fromLeft);
+ScrollReveal().reveal('.from-right', fromRight);
+ScrollReveal().reveal('.from-top', fromTop);
+
+ScrollReveal().reveal('.from-bottom', fromBottom);
+ScrollReveal().reveal('.from-bottom_50', {...fromBottom, delay: 50});
+ScrollReveal().reveal('.from-bottom_100', {...fromBottom, delay: 100});
+ScrollReveal().reveal('.from-bottom_150', {...fromBottom, delay: 150});
+ScrollReveal().reveal('.from-bottom_long', fromBottomLong);
+
+
 (function($) {
   $(document).ready(function() {
     // Toogle Menu
@@ -13,9 +48,7 @@
       e.preventDefault();
 
       const $this = $(this);
-      const tapId = $this
-        .attr('href')
-        .replace('#', '');
+      const tapId = $this.attr('href').replace('#', '');
       const $tapContent = $(`[data-tap='${tapId}']`);
 
       $('.tap-content').slideUp(600);
